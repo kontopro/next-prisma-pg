221534e1a587970ecc66692e1defaee53ea22f58
kontopro <konteap@gmail.com>
2021-11-12 19:37:37 +0200

    Layout _app wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { SessionProvider } from "next-auth/react"
+import Layout from "../components/Layout"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
   return (
+    <Layout>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+    </Layout>
+   )
 }
 
 export default MyApp
