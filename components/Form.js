ac7d793135b0e208cc3a12114bf2e89dcc65ce33
kontopro <konteap@gmail.com>
2021-11-23 18:22:24 +0200

    insert title
    custom form, leave react-form

--- components/Form.js
+++ components/Form.js
@@ -1,13 +1,26 @@
-import { useForm } from "react-hook-form";
+import { useState } from 'react';
 import styles from '../styles/Form.module.css'
 
 export default function Form({savePost}){
+
+    const [post,setPost] = useState({title:''})
+
+    const handlePost = (e) => {
+        e.preventDefault();
+        setPost({...post,[e.target.name]: e.target.value})
+    }
     
-    const {register,handleSubmit}= useForm();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(post)
+        return (savePost(post))
+    }
     
     return(
-        <form className={styles.addPost} onSubmit={ handleSubmit(savePost) }>
-        <input className={styles.addPost} placeholder="hello" {...register("title")} />
+        <form className={styles.addPost} onSubmit={ handleSubmit }>
+        <input className={styles.addPost} onChange={handlePost} placeholder="title" name='title' />
+        {/* <input className={styles.addPost} placeholder="heading" name='heading' /> */}
+        {/* <input className={styles.addPost} placeholder="paragraph" name='paragraph' /> */}
 
         <input className={styles.addPost} type="submit" value="Submit" />
         </form>
--- pages/api/insgetPost.js
+++ pages/api/insgetPost.js
@@ -1,23 +1,11 @@
 import prisma from '../../lib/prisma'
-import { useSession } from "next-auth/react"
-
-// prisma.$use(async (params, next) => {  
-
-//   if (params.model == 'Post' && params.action == 'create' ) {
-//     params.args.data.authorId = 'ckvqwal2i0084g4iwe6fd1cfk'
-//   }
-
-//   return next(params)
-
-// })
 
 export default async (req, res) => {
   
     // Αν ζητηθεί ως post, να γίνει insert
     if (req.method === 'POST')  {
-      try {
-          
-          const newPost = JSON.parse(req.body);        
+      try {          
+          const newPost = JSON.parse(req.body);      
           const toAdd = await prisma.post.create({ data: newPost });         
           res.status(200).json(toAdd);
       } 
@@ -26,11 +14,10 @@ export default async (req, res) => {
           res.status(403).json({ err: "Σφάλμα στην εισαγωγή εγγραφής (νεό ποστ)" });
       }
     }
-  // 2 σε 1, αν απλά ζητήσω το url δώσε μου όλα τα ποστ
+    // 2 σε 1, αν απλά ζητήσω το url δώσε μου όλα τα ποστ
     else 
     if (req.method === 'GET') {
-      try {
-        
+      try {        
           const allPosts = await prisma.post.findMany({
             select: {
               id: true,
